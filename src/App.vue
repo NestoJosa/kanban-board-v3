42d5787240defaaac0ce762eeea6e72ecfa42c4a
NestoJosa <59732463+NestoJosa@users.noreply.github.com>
2024-06-06 09:23:20 +0200

    Render a StatusColumn

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
-
 <template>
   <h1>Hola</h1>
+  <StatusColumn :title="'To Do'" :cardsList="cardsList" />
 </template>
+
+<script setup lang="ts">
+import StatusColumn from './components/StatusColumn.vue'
+
+const cardsList = [
+  { id: 1, title: 'Task 1', description: 'This is task 1' },
+  { id: 2, title: 'Task 2', description: 'This is task 2' },
+  { id: 3, title: 'Task 3', description: 'This is task 3' }
+]
+</script>
 
 <style scoped></style>
