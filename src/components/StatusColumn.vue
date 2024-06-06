b3e2adeebccd0e5da3e9e8457f01d91cb9809204
NestoJosa <59732463+NestoJosa@users.noreply.github.com>
2024-06-06 19:41:58 +0200

    Start using VueDraggableNext

--- src/components/StatusColumn.vue
+++ src/components/StatusColumn.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="status-column">
     <h2>{{ title }}</h2>
-    <ul>
-      <li v-for="card in cardsList" :key="card.id">
-        <TaskCard :title="card.title" :description="card.description" />
-      </li>
-    </ul>
+    <VueDraggableNext
+      class="dragArea"
+      :list="cardsList"
+      group="cards"
+      itemKey="id"
+      @start="
+        () => {
+          console.log('at-start-fired')
+        }
+      "
+      @end="
+        () => {
+          console.log('at-end-fired')
+        }
+      "
+      @change="
+        () => {
+          console.log('at-change-fired')
+        }
+      "
+    >
+      <div
+        class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center"
+        v-for="element in cardsList"
+        :key="element.id"
+      >
+        <TaskCard :title="element.title" :description="element.description" />
+      </div>
+    </VueDraggableNext>
   </div>
 </template>
 
 <script setup lang="ts">
 import TaskCard from './TaskCard.vue'
+import { VueDraggableNext } from 'vue-draggable-next'
 defineProps<{
   title: String
   cardsList: Array<{ id: number; title: string; description: string }>
