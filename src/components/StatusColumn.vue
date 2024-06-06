ca6e38ae5e0c47055a3196583d5cffec17166fb9
NestoJosa <59732463+NestoJosa@users.noreply.github.com>
2024-06-06 20:49:43 +0200

    Finish the logik for draggin cards...
    and updating the actual lists defined in App (and not just relying on the draggable lib to visually drag the cards around on screen)

--- src/components/StatusColumn.vue
+++ src/components/StatusColumn.vue
@@ -3,28 +3,14 @@
     <h2>{{ title }}</h2>
     <VueDraggableNext
       class="dragArea"
-      :list="cardsList"
+      :list="props.cardsList"
       group="cards"
       itemKey="id"
-      @start="
-        () => {
-          console.log('at-start-fired')
-        }
-      "
-      @end="
-        () => {
-          console.log('at-end-fired')
-        }
-      "
-      @change="
-        () => {
-          console.log('at-change-fired')
-        }
-      "
+      @change="handleDragEnd"
     >
       <div
         class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center"
-        v-for="element in cardsList"
+        v-for="element in props.cardsList"
         :key="element.id"
       >
         <TaskCard :title="element.title" :description="element.description" />
@@ -36,10 +22,15 @@
 <script setup lang="ts">
 import TaskCard from './TaskCard.vue'
 import { VueDraggableNext } from 'vue-draggable-next'
-defineProps<{
+const props = defineProps<{
   title: String
   cardsList: Array<{ id: number; title: string; description: string }>
 }>()
+const emit = defineEmits(['update:cardsList'])
+
+const handleDragEnd = () => {
+  emit('update:cardsList', props.cardsList)
+}
 </script>
 
 <style scoped>
