5b67d8b800532e374510b3b8299304cfc2b4442c
NestoJosa <59732463+NestoJosa@users.noreply.github.com>
2024-06-06 09:08:49 +0200

    StatusColumn renders a list of task cards

--- src/components/StatusColumn.vue
+++ src/components/StatusColumn.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="status-column">
     <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="card in cardsList" v-bind:key="card.id">
+        <TaskCard v-bind:title="card.title" v-bind:description="card.description" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+import TaskCard from './TaskCard.vue'
 defineProps<{
-  title: string
+  title: String
+  cardsList: Array<{ id: number; title: string; description: string }>
 }>()
 </script>
 
--- src/components/__tests__/StatusColumn.spec.ts
+++ src/components/__tests__/StatusColumn.spec.ts
@@ -1,11 +1,25 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import StatusColumn from '../StatusColumn.vue'
+import TaskCard from '../TaskCard.vue'
 
 describe('StatusColumn', () => {
+  const title = 'Status Column Title'
+  const cardsList = [
+    { id: 1, title: 'Task 1', description: 'Task 1 description' },
+    { id: 2, title: 'Task 2', description: 'Task 2 description' },
+    { id: 3, title: 'Task 3', description: 'Task 3 description' },
+    { id: 4, title: 'Task 4', description: 'Task 4 description' }
+  ]
+
   it('renders a title', () => {
-    const title = 'Status Column Title'
-    const wrapper = mount(StatusColumn, { props: { title } })
+    const wrapper = mount(StatusColumn, { props: { title, cardsList } })
     expect(wrapper.text()).toContain(title)
+  })
+
+  it('renders a list of task cards', () => {
+    const wrapper = mount(StatusColumn, { props: { title, cardsList } })
+    const foundTaskCards = wrapper.findAllComponents(TaskCard)
+    expect(foundTaskCards.length).toBe(cardsList.length)
   })
 })
